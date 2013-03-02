6436e7d4cae6be41c3839e6f6d7f0257cac62d2b
Fabio Battaglia <hkzlabnet@gmail.com>
2013-03-03 00:34:10 +0100

    TWILIB: Update example

--- ds1307-driver/main.c
+++ ds1307-driver/main.c
@@ -19,22 +19,30 @@ int main(void) {
     stdout = &uart_output;
     stdin  = &uart_input;
 
+	uint8_t readData[8];
+	uint8_t writeData[7];
+
+	writeData[0] = 0x00;
+	writeData[1] = 0x33;
+	writeData[2] = 0x00;
+	writeData[3] = 0x7;
+	writeData[4] = 0x03;
+	writeData[5] = 0x03;
+	writeData[6] = 0x13;
+
 	I2C_Config *masterConfig = I2C_buildDefaultConfig();
 	I2C_Device dsDevice;
 	dsDevice.id = 0xD;
 	dsDevice.address = 0;
 	I2C_masterBegin(masterConfig);
 
-	uint8_t dstData[2]; dstData[0] = 0x00; dstData[1] = 0x00;
-	uint8_t regData[2];
-
-	I2C_masterWriteRegisterByte(&dsDevice, 0x00, dstData, 2);
-	I2C_masterReadRegisterByte(&dsDevice, 0x00, regData, 2);
+	//I2C_masterWriteRegisterByte(&dsDevice, 0x00, writeData, 7);
+	I2C_masterReadRegisterByte(&dsDevice, 0x00, readData, 8);
 
 	while (1) {
 		_delay_ms(3000);
-		fprintf(stdout, "Loop! 0x%.2X  0x%.2X\n", regData[0], regData[1]);
-		I2C_masterReadRegisterByte(&dsDevice, 0x00, regData, 2);
+		fprintf(stdout, "%.2X %.2X %.2X %.2X %.2X %.2X %.2X %.2X\n", readData[0], readData[1], readData[2], readData[3], readData[4], readData[5], readData[6], readData[7]);
+		I2C_masterReadRegisterByte(&dsDevice, 0x00, readData, 8);
 	}
 
     return 0;
