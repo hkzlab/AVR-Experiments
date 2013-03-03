5f7f57476534b00f84919aac9c0bb44e69693dab
Fabio Battaglia <hkzlabnet@gmail.com>
2013-03-03 18:10:49 +0100

    DS1307: Example now prints time on LCD

--- samples/ds1307-driver/main.c
+++ samples/ds1307-driver/main.c
@@ -17,19 +17,29 @@
 #include "twilib.h"
 #include "ds1307.h"
 
+#include "hd44780-avr-interface.h"
+#include "hd44780-highlevel.h"
+
 volatile uint8_t interruptReceived = 0;
 
 int main(void) {
+	char lcdString[80];
+
 	// Initialize serial port for output
     uart_init();
     stdout = &uart_output;
     stdin  = &uart_input;
 
-	DS1307_ToD time;
-
+	DDRB = 0xFF; // Set port B as output!
+	PORTB = 0x00; // And zero it
+	hd44780_connection *conn_struct = hd44780_createConnection(&PORTB, 1, &PORTB, 0, &PORTB, 1, &PORTB, 2);
+	hd44780_driver *connDriver = hd44780_hl_createDriver(TMBC20464BSP_20x4, conn_struct, (uint8_t (*)(void*))hd44780_initLCD4Bit, (void (*)(void*, uint16_t))hd44780_sendCommand);
+	hd44780_hl_init(connDriver, 0, 0);
+	
 	I2C_Config *masterConfig = I2C_buildDefaultConfig();
 	I2C_masterBegin(masterConfig);
 
+	DS1307_ToD time;	
 	DS1307_setSQW(1, 0, DS1307_SQW_1Hz);
 	DS1307_readToD(&time);
 
@@ -46,7 +56,8 @@ int main(void) {
 		if (interruptReceived) {
 			interruptReceived = 0;
 			DS1307_readToD(&time);
-			fprintf(stdout, "%.2u-%.2u-%.4u %.2u:%.2u:%.2u\n", time.dayOfMonth, time.month, time.year, time.hours, time.minutes, time.seconds);
+			sprintf(lcdString, "%.2u-%.2u-%.4u\n%.2u:%.2u:%.2u", time.dayOfMonth, time.month, time.year, time.hours, time.minutes, time.seconds);
+			hd44780_hl_printText(connDriver, 0, 0, lcdString);
 		}
 	}
 
