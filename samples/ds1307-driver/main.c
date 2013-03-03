062b70a3682ed1fa21aa763e24460f869b59fa36
Fabio Battaglia <hkzlabnet@gmail.com>
2013-03-03 22:07:22 +0100

    DS1307 use PCINT interrupt

--- samples/ds1307-driver/main.c
+++ samples/ds1307-driver/main.c
@@ -30,6 +30,15 @@ int main(void) {
     stdout = &uart_output;
     stdin  = &uart_input;
 
+	// Setup ADC
+	// See here: http://www.avrbeginners.net/
+	// http://www.avrfreaks.net/index.php?name=PNphpBB2&file=viewtopic&t=56429
+	// http://www.protostack.com/blog/2011/02/analogue-to-digital-conversion-on-an-atmega168/
+//	ADCSRA = (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0) | (1 << ADEN);
+//	ADMUX = (1 << REFS0) | 0x7;
+
+
+	// Setup the LCD
 	DDRB = 0xFF; // Set port B as output!
 	PORTB = 0x00; // And zero it
 	hd44780_connection *conn_struct = hd44780_createConnection(&PORTB, 1, &PORTB, 0, &PORTB, 1, &PORTB, 2);
@@ -43,10 +52,11 @@ int main(void) {
 	DS1307_setSQW(1, 0, DS1307_SQW_1Hz);
 	DS1307_readToD(&time);
 
-	DDRD = DDRD & 0xCF;
-	// See here for interrupts: http://www.protostack.com/blog/2010/09/external-interrupts-on-an-atmega168/
-	EIMSK |= 1 << INT1;
-	EICRA |= 1 << ISC11;
+	DDRC &= 0x7F;
+	PORTC |= 0x80;
+	// See here for interrupts http://www.protostack.com/blog/2010/09/external-interrupts-on-an-atmega168/
+	PCICR |= 1 << PCIE2;
+	PCMSK2 |= 1 << PCINT23;
 	sei();
 
 	_delay_ms(500);
@@ -58,12 +68,19 @@ int main(void) {
 			DS1307_readToD(&time);
 			sprintf(lcdString, "%.2u-%.2u-%.4u\n%.2u:%.2u:%.2u", time.dayOfMonth, time.month, time.year, time.hours, time.minutes, time.seconds);
 			hd44780_hl_printText(connDriver, 0, 0, lcdString);
+
+
 		}
+/*			ADCSRA |= (1 << ADSC);
+			while (ADCSRA & (1 << ADSC)); // Busy wait...
+			uint16_t analogRead = (ADCH << 8 | ADCL);
+			fprintf(stdout, "ANALOG %.4X\n", analogRead);
+*/
 	}
 
     return 0;
 }
 
-ISR(INT1_vect) {
+ISR(PCINT2_vect) {
 	interruptReceived = 1;
 }
