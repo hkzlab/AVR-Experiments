e6c4983c72214e0dadb3f38985884604739fd41d
Fabio Battaglia <hkzlabnet@gmail.com>
2013-03-16 12:22:57 +0100

    Use centralized parameter for programmer port

--- samples/common.mk
+++ samples/common.mk
@@ -1,5 +1,8 @@
 # CPU Frequency of the atmega processor
 F_CPU = 10000000
 
+# Device used for programming
+AVRDUDE_PORT = /dev/cu.usbserial
+
 # Baud rate used when programming through avr109 protocol
 AVRDUDE_BRATE = 115200
