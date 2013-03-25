ca4fc7c3dbdd434320397f3adc0c794687b92c3f
Fabio Battaglia <hkzlabnet@gmail.com>
2013-03-25 19:18:35 +0100

    New makefile structure

---
+++ samples/1284p-common.mk
@@ -0,0 +1,6 @@
+# CPU Frequency of the atmega processor
+F_CPU = 10000000
+
+MCU = atmega1284p
+
+include ../common.mk
---
+++ samples/328p-common.mk
@@ -0,0 +1,6 @@
+# CPU Frequency of the atmega processor
+F_CPU = 16000000
+
+MCU = atmega328p
+
+include ../common.mk
