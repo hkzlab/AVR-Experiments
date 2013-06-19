7ea157ea0b71051903ac231d4e1e7907f212440c
Fabio Battaglia <hkzlabnet@gmail.com>
2013-06-19 22:12:04 +0200

    Continue preparing keyboard init code

--- libs/ps2_keyb/ps2_keyb.c
+++ libs/ps2_keyb/ps2_keyb.c
@@ -1,2 +1,34 @@
 #include "ps2_keyb.h"
 
+#include <avr/io.h>
+
+// Data port
+static volatile uint8_t *dPort, *dPin, *dDir;
+static uint8_t dPNum; // Data port pin (leg) number
+
+// Clock port
+static volatile uint8_t *cPort, *cPin, *cDir;
+static uint8_t cPNum;
+
+// See http://avrprogrammers.com/example_avr_keyboard.php
+// http://elecrom.wordpress.com/2008/02/12/avr-tutorial-2-avr-input-output/
+void ps2keyb_init(volatile uint8_t *dataPort, volatile uint8_t *dataDir, volatile uint8_t *dataPin, uint8_t pNum) {
+	dPort = dataPort;
+	dPin = dataPin;
+	dDir = dataDir;
+	dPNum = pNum;
+
+	cPort = &PORTD;
+	cPin = &PIND;
+	cDir = &DDRD;
+	cPNum = 2; // PD2
+
+	// Prepare data port
+	*dDir &= ~(1 << dPNum); // KB Data line set as input
+	*dPort |= (1 << dPNum); // Pull-up resistor on data line
+
+	// Prepare clock port
+	*cDir &= ~(1 << cPNum); // KB Clock line set as input
+	*cPort |= (1 << cPNum); // Pull-up resistor on clock line
+}
+
--- libs/ps2_keyb/ps2_keyb.h
+++ libs/ps2_keyb/ps2_keyb.h
@@ -4,7 +4,8 @@
 #include <stdint.h>
 
 // Clock port MUST be the one corresponding to INT0 !
+
 // Data port can be set at will
-void ps2keyb_init(volatile uint8_t *clockPort, volatile uint8_t *dataPort);
+void ps2keyb_init(volatile uint8_t *dataPort, volatile uint8_t *dataDir, volatile uint8_t *dataPin, uint8_t pNum);
 
 #endif /* _AVR_PS2_KEYB_HEADER_ */
