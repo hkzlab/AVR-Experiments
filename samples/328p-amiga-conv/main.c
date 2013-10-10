4c31d329e0a29704d75033864e115984981219d9
Fabio Battaglia <hkzlabnet@gmail.com>
2013-10-11 01:04:49 +0200

    Pull high unused pins in pc-amiga kb converter

--- samples/328p-amiga-conv/main.c
+++ samples/328p-amiga-conv/main.c
@@ -16,6 +16,19 @@
 
 
 int main(void) {
+	// Set the pull-up resistor to all unused I/O ...
+	DDRB &= 0x03;
+	PORTB |= 0xFC;
+	
+	DDRC &= 0xC0;
+	PORTC |= 0x3F;
+
+	DDRD &= 0x0C;
+	PORTD |= 0xF3;
+
+	_delay_us(100);
+
+	// Initialization of PS/2 and Amiga interface
 	amikbd_setup(&PORTB, &DDRB, 0);
 
 	ps2keyb_init(&PORTB, &DDRB, &PINB, 1);
