a95082fb86baefc212917539aea668cfba14e10e
Fabio Battaglia <hkzlabnet@gmail.com>
2015-04-25 15:43:41 +0200

    Add empty rfm12b module

---
+++ libs/rfm12b/rfm12b.c
@@ -0,0 +1,12 @@
+#include "rfm12b.h"
+
+#include "spi.h"
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
---
+++ libs/rfm12b/rfm12b.h
@@ -0,0 +1,14 @@
+#ifndef _RFM12B_HEADER_
+#define _RFM12B_HEADER_
+
+#include <avr/io.h>
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif /* _RFM12B_HEADER_ */
